b6da1ff9193f0dcaed2923f7dcc6aed4c5480144
Dmitry Ch <chapliuk@dmitry.by>
2021-04-15 14:01:44 +0300

    import menu block js

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,1 +1,2 @@
 import './admin/migrate';
+import './admin/import-menu-block';
---
+++ src/js/admin/import-menu-block.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let importMenuBlockBtn = document.querySelector('.gm-import-notice-btn');
+
+  function importMenuBlockAction (e) {
+    e.preventDefault();
+
+    let wpMediaObj;
+
+    if (wpMediaObj) {
+      wpMediaObj.open();
+      return;
+    }
+
+    wpMediaObj = wp.media({
+      title: 'Upload Menu Block ZIP package',
+      multiple: false,
+      library: {type: 'application/octet-stream, application/zip'}
+    });
+
+    wpMediaObj.on('select', () => {
+      let uploadedZIP = wpMediaObj
+        .state()
+        .get('selection')
+        .first()
+        .toJSON();
+
+      // uploadedZIP.url;
+
+      let actionValue = 'gm_import_menu_block_from_zip_url';
+      let gmImportNonce = document.querySelector('#gm-nonce-import-menu-block-field');
+      const data = {
+        action: actionValue,
+        zipUrl: uploadedZIP.url,
+        gm_nonce: gmImportNonce.value
+      };
+      const params = new URLSearchParams(data);
+
+      axios.post(ajaxurl, params)
+        .then(() => window.location.reload(false))
+        .catch(() => alert('Ajax error'));
+
+    });
+    wpMediaObj.open();
+    return false;
+  }
+
+  if (importMenuBlockBtn !== null) {
+    importMenuBlockBtn.addEventListener('click', importMenuBlockAction);
+  }
+
+});
